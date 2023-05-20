9c20ecf8555412b5267518b677ef2e1c5575820c
NicolasAlejo <134144326+NicolasAlejo@users.noreply.github.com>
2023-05-20 09:22:10 -0300

    Update Obrero.cs

--- proyecto1/proyecto1/Clases/Obrero.cs
+++ proyecto1/proyecto1/Clases/Obrero.cs
@@ -16,8 +16,64 @@ namespace proyecto1.Clases
 	/// </summary>
 	public class Obrero
 	{
-		public Obrero()
-		{
-		}
+		// variables de instacia / atributos
+        	private string nombre, apellido, cargo;
+       		private int legajo, dni;
+        	private int grupoAlQuePertenece;
+			
+			//constructor
+			
+			public Obrero(string nombre, string apellido, string cargo, int legajo, int dni, int grupoAlQuePertenece)
+			{
+				this.nombre = nombre;
+				this.apellido = apellido;
+				this.cargo = cargo;
+				this.legajo = legajo;
+				this.dni = dni;
+				this.grupoAlQuePertenece = grupoAlQuePertenece;
+			}
+			
+			//propiedades
+			
+			public string Nombre
+			{
+				set { nombre = value;}
+				get { return nombre;}
+			}
+			
+			public string Apellido
+			{
+				set { apellido = value;}
+				get { return apellido;}
+			}
+			
+			public string Cargo
+			{
+				set { cargo = value; } 
+				get { return cargo;}
+			}
+			
+			public int Legajo
+      			{
+           		 set { legajo = value; }
+           		 get { return legajo; }
+        		} 
+			
+			public int Dni
+      			{
+         	   	set { dni = value; }
+        	    	get { return dni; }
+        		}
+			 
+			public int GrupoAlQuePertenece
+      			{
+            		set { dni = value; }
+            		get { return dni; }
+        		}
+			
+			
+			
+			// METODOS
+			
 	}
 }
